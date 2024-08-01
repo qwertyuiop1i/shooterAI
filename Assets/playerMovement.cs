9431d86ffd82e07b6efd4bad85b5cbfcce8b7117
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-08-01 09:55:57 -0400

    Commit #1

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized * speed;
+        rb.velocity = movement;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = mousePos - transform.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0f,0f, angle);
     }
 }
+
---
+++ Assets/playerShoot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float fireRate = 3f;
+    public float bulletSpeed = 10f;
+
+    private float nextTimeToFire = 0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
+        {
+            Shoot();
+            nextTimeToFire = Time.time + 1f / fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation).GetComponent<Rigidbody2D>().velocity = firePoint.up * bulletSpeed;
+    }
+}
