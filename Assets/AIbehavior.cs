1a0b4738d61dee2fd0c766b7acbd796c0da68c29
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-08-01 15:25:52 -0400

    Commit #5

--- Assets/AIbehavior.cs
+++ Assets/AIbehavior.cs
@@ -10,11 +10,18 @@ public class AIbehavior : MonoBehaviour
     [SerializeField]
     private AIshoot shoot;
 
-    public GameObject player;
+    public GameObject ?player;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         Vector2 dir = new Vector2(0f, 0f);
 
@@ -27,18 +34,21 @@ public class AIbehavior : MonoBehaviour
             //A. determine how long object
             if (player.GetComponent<Rigidbody2D>().velocity.magnitude == 0f)
             {
-                rb.rotation = Mathf.Atan2(player.transform.position.y, player.transform.position.x)*Mathf.Rad2Deg;
-            }
 
+                rb.rotation = Mathf.Atan2(player.transform.position.y-transform.position.y, player.transform.position.x-transform.position.x) * Mathf.Rad2Deg-90f;
+            }
+            else
+            {
+                Vector2 playerVel=player.GetComponent<Rigidbody2D>().velocity;
+                float estimatedTime = Vector2.Distance(shoot.firePoint.position, player.transform.position) / shoot.bulletSpeed;
+                Vector2 futurePos = predictFuturePos(player.transform.position, playerVel, estimatedTime);
+                rb.rotation = Mathf.Atan2(futurePos.y - transform.position.y, futurePos.x - transform.position.x)*Mathf.Rad2Deg-90f;
+                //predictFuturePos(player.transform, playerVel,t)
+            }
+            rb.angularVelocity = 0f;
             shoot.Shoot();
         }
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public Vector2 predictFuturePos(Vector2 initPos, Vector2 velocity, float time)
